7513d478a7f0b89006a71ccd01aad43759e113d6
hector <hectorcorcuera09@gmail.com>
2019-05-23 19:58:24 +0200

    subiendo archivo linea al repositorio

---
+++ linea.sql
@@ -0,0 +1,14 @@
+create table linea(
+	num_ped		number(10),
+	num_lin		number(2),
+	fecha_par	date,
+	fecha_des	date,
+	carga		number(4),
+	cod_ruta	number(8),
+	mat_rem		varchar2(7),			
+	CONSTRAINT cp_linea PRIMARY KEY (num_ped, mun_lin),
+	CONSTRAINT ca_linea_pedido FOREIGN KEY (num_ped) REFERENCES pedido (num_ped),
+	CONSTRAINT ca_linea_ruta FOREIGN KEY (cod_ruta) REFERENCES ruta (cod_ruta),
+	CONSTRAINT ca_linea_remolque FOREIGN KEY (mat_rem) REFERENCES remolque (mat_rem)
+)
+/
